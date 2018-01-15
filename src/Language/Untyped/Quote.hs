f9d95b0e63c48d0a69ec8219aec0dd6ee306ac6b
Henry Till <henrytill@gmail.com>
2018-01-14 19:17:31 -0500

    L.U.Quote: restructure

--- src/Language/Untyped/Quote.hs
+++ src/Language/Untyped/Quote.hs
@@ -6,7 +6,8 @@ import           Language.Haskell.TH.Quote
 import           Language.Untyped.Lambda   (Term (TmMetaVar))
 import qualified Language.Untyped.Lexer    as Lexer
 import           Language.Untyped.Parser   (termP)
-import           Text.Parsec
+import           Text.Parsec               (SourcePos, eof, runParser,
+                                            setPosition)
 import           Text.Parsec.Pos           (newPos)
 import           Text.Parsec.String        (Parser)
 
@@ -17,14 +18,14 @@ getSourcePos = f <$> TH.location
     f :: TH.Loc -> SourcePos
     f loc = uncurry (newPos (TH.loc_filename loc)) (TH.loc_start loc)
 
-parseExp :: Monad m => SourcePos -> String -> m Term
-parseExp pos str = parseOrError (setPosition pos *> topLevel termP) "untyped lambda calculus" str
-  where
-    parseOrError :: Monad m => Parser Term -> String -> String -> m Term
-    parseOrError p name str = either (error . show) return (runParser p () name str)
+parseOrError :: Monad m => Parser Term -> String -> String -> m Term
+parseOrError p name str = either (error . show) return (runParser p () name str)
 
-    topLevel :: Parser Term -> Parser Term
-    topLevel p = Lexer.spaces *> p <* eof
+topLevel :: Parser Term -> Parser Term
+topLevel p = Lexer.spaces *> p <* eof
+
+parseTerm :: Monad m => SourcePos -> String -> m Term
+parseTerm pos str = parseOrError (setPosition pos *> topLevel termP) "untyped lambda calculus" str
 
 antiExpLambda :: Term -> Maybe (TH.Q TH.Exp)
 antiExpLambda (TmMetaVar _ v) = Just (TH.varE (TH.mkName v))
@@ -36,15 +37,15 @@ antiPatLambda _               = Nothing
 
 quoteExpLambda :: String -> TH.Q TH.Exp
 quoteExpLambda str = do
-  l <- getSourcePos
-  c <- parseExp l str
-  dataToExpQ (const Nothing `extQ` antiExpLambda) c
+  pos  <- getSourcePos
+  term <- parseTerm pos str
+  dataToExpQ (const Nothing `extQ` antiExpLambda) term
 
 quotePatLambda :: String -> TH.Q TH.Pat
 quotePatLambda str = do
-  l <- getSourcePos
-  c <- parseExp l str
-  dataToPatQ (const Nothing `extQ` antiPatLambda) c
+  pos  <- getSourcePos
+  term <- parseTerm pos str
+  dataToPatQ (const Nothing `extQ` antiPatLambda) term
 
 untyped :: QuasiQuoter
 untyped = QuasiQuoter
